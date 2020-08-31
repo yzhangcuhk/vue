9f33d11f9906cd20aca4c0d00cce843c9f2ea3d4
caishaodong <1146631758@qq.com>
2020-08-31 15:28:06 +0800

    数据备份

---
+++ admin-server/src/main/resources/data/data.sql
@@ -0,0 +1,90 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 127.0.0.1
+ Source Server Type    : MySQL
+ Source Server Version : 50731
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : test
+
+ Target Server Type    : MySQL
+ Target Server Version : 50731
+ File Encoding         : 65001
+
+ Date: 31/08/2020 15:26:21
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sys_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_menu`;
+CREATE TABLE `sys_menu`  (
+  `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `pid` bigint(11) NOT NULL COMMENT '父级id',
+  `name` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '名称',
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '地址',
+  `type` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'menu' COMMENT '类型（菜单：menu，按钮：button）',
+  `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态（0：可用，1：禁用）',
+  `is_deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除（0：正常，1：删除）',
+  `gmt_create` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `gmt_modified` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `inx_pid`(`pid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统菜单' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user`  (
+  `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '姓名',
+  `login_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '登录名',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '密码',
+  `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态（0：正常，1：注销，2：冻结）',
+  `is_deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除（0：正常，1：删除）',
+  `gmt_create` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `gmt_modified` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统用户' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES (1, '超级管理员', 'admin', '123456', 0, 0, '2020-08-06 18:08:05', '2020-08-06 18:21:56');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '姓名',
+  `mobile` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '手机号',
+  `email` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '邮箱',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '密码',
+  `age` int(4) UNSIGNED NULL DEFAULT NULL COMMENT '年龄',
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '住址',
+  `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态（0：正常，1：注销，2：冻结）',
+  `is_deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除（0：正常，1：删除）',
+  `gmt_create` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `gmt_modified` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, '胡歌', '18988880000', '', '123456', NULL, '', 0, 1, '2020-08-06 17:12:43', '2020-08-06 17:42:47');
+INSERT INTO `user` VALUES (2, '王一博', '18988880001', '', '123456', NULL, '', 0, 0, '2020-08-06 17:20:37', '2020-08-06 17:20:37');
+INSERT INTO `user` VALUES (3, '张艺兴', '18988880002', '', '123456', NULL, '', 0, 0, '2020-08-06 17:20:45', '2020-08-06 17:20:45');
+INSERT INTO `user` VALUES (4, '钟汉良', '18988880004', '', '123456', NULL, '', 0, 0, '2020-08-06 17:20:51', '2020-08-06 17:20:51');
+INSERT INTO `user` VALUES (5, '王嘉尔', '18988880005', '', '123456', NULL, '', 0, 0, '2020-08-06 17:20:58', '2020-08-06 17:20:58');
+INSERT INTO `user` VALUES (6, '黄渤', '18988880006', '', '123456', NULL, '', 0, 0, '2020-08-06 17:29:15', '2020-08-06 17:29:15');
+INSERT INTO `user` VALUES (7, '霍建华', '18988880007', '', '123456', NULL, '', 0, 0, '2020-08-06 17:29:24', '2020-08-06 17:29:24');
+INSERT INTO `user` VALUES (8, '王凯1', '18988880008', '', '123457', NULL, '', 0, 0, '2020-08-06 09:29:31', '2020-08-06 09:29:31');
+
+SET FOREIGN_KEY_CHECKS = 0;
